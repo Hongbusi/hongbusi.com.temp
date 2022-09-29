9456f5e20d155eabbe53422540d18845f2b5652a
Hongbusi <coderhbs@gmail.com>
2022-09-29 17:54:41 +0800

    chore: update logo

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { isDark } from '../composables/dark'
+</script>
+
+<template>
+  <header>
+    <div class="container px-4 py-2 mx-auto">
+      <div>
+        <img v-if="isDark" src="/logo-dark.svg" class="h-56px" alt="logo">
+        <img v-else src="/logo.svg" class="h-56px" alt="logo">
+      </div>
+    </div>
+  </header>
+</template>
+
