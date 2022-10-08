aff71959201f8d875aa9a08dce9bf15fcae90c86
Hongbusi <coderhbs@gmail.com>
2022-10-08 17:28:13 +0800

    chore: update header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,28 @@
 <script setup lang="ts">
-import { isDark } from '../composables/dark'
+import { Github, Moon, Sunny } from '@hongbusi/icons-vue'
+import { isDark, toggleDark } from '~/composables/dark'
 </script>
 
 <template>
-  <header>
-    <div class="container px-4 py-2 mx-auto">
-      <div>
-        <img v-if="isDark" src="/logo-dark.svg" class="h-56px" alt="logo">
+  <header class="sticky top-0 z-40 w-full backdrop-blur">
+    <div class="flex justify-between items-center px-2 mx-auto container">
+      <div class="py-2">
+        <img v-if="isDark" src="/logo-dark.svg" class="h-46px" alt="logo">
         <img v-else src="/logo.svg" class="h-56px" alt="logo">
+      </div>
+      <div class="text-xl">
+        <button class="mx-2 !outline-none" title="切换深色模式" @click="toggleDark()">
+          <Icon>
+            <Moon v-if="isDark" />
+            <Sunny v-else />
+          </Icon>
+        </button>
+
+        <a class="mx-2" rel="noreferrer" href="https://github.com/Hongbusi/Hongbusi" target="_blank" title="GitHub">
+          <Icon>
+            <Github />
+          </Icon>
+        </a>
       </div>
     </div>
   </header>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -2,11 +2,8 @@
   <main class="text-gray-700 dark:text-gray-200">
     <Header />
 
-    <router-view />
-    <Footer />
-
-    <div class="mt-5 mx-auto text-center opacity-25 text-sm">
-      [Default Layout]
+    <div class="px-2 mx-auto max-w-1024px">
+      <router-view />
     </div>
   </main>
 </template>
