5b59cc563a0302971163e5782682996ec3fbdbfe
Hongbusi <coderhbs@gmail.com>
2022-10-11 18:51:14 +0800

    chore: update header icons

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,7 +9,7 @@ const { y } = useWindowScroll()
     class="sticky top-0 z-40 w-full bg-white dark:bg-#121212 shadow-sm shadow-black/10 transition-transform duration-200 ease-in-out"
     :class="{ '-translate-y-full': y >= 50 }"
   >
-    <nav class="flex justify-between items-center px-4 py-2 mx-auto container">
+    <div class="flex justify-between items-center px-4 py-2 mx-auto container">
       <div class="flex items-center space-x-2">
         <div class="w-40px h-40px bg-black" />
         <h1 class="text-2xl">
@@ -17,17 +17,25 @@ const { y } = useWindowScroll()
         </h1>
       </div>
 
-      <div class="text-xl">
-        <button class="mx-2 !outline-none" title="切换深色模式" @click="toggleDark()">
+      <nav class="flex items-center space-x-3 text-xl">
+        <router-link to="/posts">
+          Blog
+        </router-link>
+
+        <a rel="noreferrer" href="https://twitter.com/Hongbusi" target="_blank" title="GitHub">
+          <div class="i-hbs-twitter" />
+        </a>
+
+        <a rel="noreferrer" href="https://github.com/Hongbusi" target="_blank" title="GitHub">
+          <div class="i-hbs-github" />
+        </a>
+
+        <button class="flex justify-center items-center w-8 h-8 !outline-none rounded-full hover:bg-#ebedf0 dark:hover:bg-#444950" title="切换深色模式" @click="toggleDark()">
           <div v-if="isDark" class="i-hbs-moon" />
           <div v-else class="i-hbs-sunny" />
         </button>
-
-        <a class="mx-2" rel="noreferrer" href="https://github.com/Hongbusi" target="_blank" title="GitHub">
-          <div class="i-hbs-github" />
-        </a>
-      </div>
-    </nav>
+      </nav>
+    </div>
   </header>
 </template>
 
